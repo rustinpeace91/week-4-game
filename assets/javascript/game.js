e5e40c4bd7106e2dbe906cdef57bb0934dc7ce65
Isaac Lubanko <isaaclubanko@MacBook-Pro-2.fios-router.home>
2018-02-25 18:47:57 -0500

    can now select character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,26 @@
 
 
 //creates an array of jedi, for purposes of determining characters
-var jedi = ["obiWan", "lukeSkywalker", "darthVader", "theEmperor"]
+var jedi = ["obiWan", "lukeSkywalker", "darthVader", "theEmperor"];
+//array of jedi ID names as they relate to the DOM
+var jediDOM = ["#obi-wan", "#luke-skywalker", "#darth-vader", "#the-emperor"]
+
+//array of jedi objects if nessary
+//var jediObj = [obiWan, lukeSkywalker, darthVader, theEmperor];
+
+//condition of whether a jedi has been chosen
 var jediChosen = false;
+//keeps track of the jedi you chose
 var yourJedi;
-var currentEnemy; 
+//keeps track of the jedi you are fighting
+var currentEnemy;
 
 //objects
 var obiWan = {
-    name:"obi wan",
-    dom: "obi-wan",
+    name:"obiWan",
+    dom: "#obi-wan",
     HP: 120,
+    mainCharacter: false,
     baseAttack: 8,
     counterAttack: 8,
 
@@ -28,19 +38,95 @@ var obiWan = {
         //checks to see HP left
     }
 
-
 };
+
+var darthVader = {
+    name: "darthVader",
+    dom: "#darth-vader",
+    HP:140,
+    baseAttack:9,
+    counterAttack:9
+}
+
+var lukeSkywalker = {
+    name: "lukeSkywalker",    
+    dom: "#luke-skywalker",
+    HP:140,
+    baseAttack:9,
+    counterAttack:9
+}
+
+var theEmperor = {
+    name: "theEmperor",
+    dom: "#the-emperor",
+    HP:140,
+    baseAttack:9,
+    counterAttack:9
+}
 
 //functions
 
+//function for sorting your enemies
+function enemySort(){
+    for(i = 0; i < jedi.length; i++){
+        if(jedi[i] != yourJedi.name.toString()) {
+            var element = $(jediDOM[i]).detach();
+            $("#enemies").append(element); 
+        }
+    }
+}
 
 
 
 //click event to select character to initiate the game
-$("#obi-wan").on("click", function(){
-    alert("I've been clicked");
-});
+$(document).ready(function(){
+
+        $("#obi-wan").on("click", function(){
+            if(jediChosen == false){
+                var element = $("#obi-wan").detach();
+                $("#your-character").append(element);
+                yourJedi = obiWan;
+                enemySort();
+                jediChosen = true;
+            }
+        });
+
+        $("#darth-vader").on("click", function(){
+            if(jediChosen == false){
+                var element = $("#darth-vader").detach();
+                $("#your-character").append(element);
+                yourJedi = darthVader;
+                enemySort();
+                jediChosen = true;
+            };
+        });
+        
+        $("#luke-skywalker").on("click", function(){
+            if(jediChosen == false){
+                var element = $("#luke-skywalker").detach();
+                $("#your-character").append(element);
+                yourJedi = lukeSkywalker;
+                enemySort();
+                jediChosen = true;
+            }
+        });
+        
+        $("#the-emperor").on("click", function(){
+            if(jediChosen == false){
+                var element = $("#the-emperor").detach();
+                $("#your-character").append(element);
+                yourJedi = theEmperor;
+                enemySort();
+                jediChosen = true;
+            };
+        });
+
+})
+
 //moves selected character to "your character" id, moves rest to "enemies" id
+
 //selects object based on character picked
+
 //attack button function. determines if attack button has been previously clicked, determines if an attack can be carried out as a result
+
 //carries out attacks, counter attacks and HP 
