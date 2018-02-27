8609ffda9176b823e535b92a756946006bb7f51c
Isaac Lubanko <isaaclubanko@MacBook-Pro-2.fios-router.home>
2018-02-26 23:57:05 -0500

    added more comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,7 @@ var jediChosen = false;
 var yourJedi;
 //keeps track of the jedi you are fighting
 var currentEnemy;
-
+//keeps track of all the enemies defeated
 var winCounter = 0;
 
 //objects
@@ -33,6 +33,7 @@ var obiWan = {
     //counter attack (does not increase)
     counterAttack: 8,
     defeated:false,
+    //updates HP on the DOM every time damage is taken
     update: function(){
         $("#obi-wan p").text(this.HP);
     },
@@ -43,13 +44,14 @@ var obiWan = {
         this.defeated = true;
         currentEnemy = undefined;
     },
-
+    //resets character attributes after the game is over
     reset:function(){
         this.HP = 120;
         this.mainCharacter = false;
         this.enemy = false;
         this.baseAttack = 8;
         this.defeated = false;
+        //moves the character icon back to it's original spot
         var element = $(jediDOM[0]).detach();
         $("#start-characters").append(element);
         $("#" + this.dom).removeClass("hidden");
@@ -208,8 +210,10 @@ function isEnemy(chosen){
     }
 };
 
+//selects the enemy that you clicked
 function selectCurrentEnemy(chosen){
-
+    //ties the id you clicked to it's corresponding object
+    //cylces throug the jediObj array, matches the .dom property with the id of the clicked item
     for(i = 0; i < jediObj.length; i++){
         if(jediObj[i].dom === chosen){
             var element = $(jediDOM[i]).detach();
@@ -219,37 +223,52 @@ function selectCurrentEnemy(chosen){
     };
 };
 
+//attack function 
 function attack() {
+    //subtacts your base attack from the enemies HP
     currentEnemy.HP -= yourJedi.baseAttack;
+    //subtracts the enemy's counter attack from your HP
     yourJedi.HP -= currentEnemy.counterAttack;
+    //increas your attack
     yourJedi.baseAttack += yourJedi.counterAttack;
     console.log("Your HP: " + yourJedi.HP + " Enemy HP:  " + currentEnemy.HP);
+    //runs the update method
     yourJedi.update();
+    //runs the update method for your enemy
     currentEnemy.update();
+    //checks to see if you have defeated the enemy
     if(currentEnemy.HP <= 0) {
         currentEnemy.defeat();
         checkWin();
+    //checks to see if you have lost all your HP     
     } else if (yourJedi.HP <= 0) {
         loseGame();
     }
 
 }
 
+//checks to see if you have defeated all your enemies
 function checkWin(){
     winCounter = 0;
-
+    //cylces through the object array. checks the defeated status of each jedi
+    //if three of the jedis have a defeated property of "true", it is assumed that you have won
+    //winCounter is reset because the function is run each time a jedi is defeated.
     for(i = 0; i < jediObj.length; i++){
         if(jediObj[i].defeated === true){
-            winCounter++; //THIS IS HTE ISSUE!!!!!!!!!!!!!!!
+            winCounter++; 
         }
     }
     if(winCounter >= 3){
-        alert("you win!");
-        $("#reset").removeClass("hidden");
+        winGame();
         winCounter = 0;
-
     }
 
+}
+
+//win and losegame function. simply displas a message and displays the reset button
+function winGame(){
+    alert("you win!");
+    $("#reset").removeClass("hidden");
 }
 
 function loseGame(){
@@ -258,10 +277,13 @@ function loseGame(){
 
 }
 
+//runs if the reset button is pushed
 function resetGame(){
+    //resets global variables
     jediChosen = false;
     yourJedi = undefined;
     currentEnemy = undefined;
+    //cycles through jedi object array. runs the reset method for each jedi
     for(i = 0; i<jediObj.length; i++){
         jediObj[i].reset();
     }
@@ -289,19 +311,20 @@ $(document).ready(function(){
         if(currentEnemy === undefined && isEnemy(chosen) == true) {
             currentEnemy = selectCurrentEnemy(chosen);;
         } 
-
+        
+        //condition to display the attack button if the enemy has been chosen
         if(currentEnemy != undefined && currentEnemy.dom === chosen){
             $("#attack").removeClass("hidden");
         }
 
 
     });
-
+    //click event for attack button
     $("#attack").on("click", function(){
 
         attack();
     });
-
+    //click event for reset button
     $("#reset").on("click", function(){
 
         resetGame();
@@ -310,10 +333,3 @@ $(document).ready(function(){
 
 })
 
-//moves selected character to "your character" id, moves rest to "enemies" id
-
-//selects object based on character picked
-
-//attack button function. determines if attack button has been previously clicked, determines if an attack can be carried out as a result
-
-//carries out attacks, counter attacks and HP 
